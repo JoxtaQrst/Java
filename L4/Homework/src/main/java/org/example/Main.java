48700782b32ebdb243f084555ec2c30f19719f74
JoxtaQrst <pintea2002@gmail.com>
2023-03-28 15:33:49 +0300

    Added Laboratory4 as a MAVEN project.

---
+++ L4/Homework/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student>studentList=Arrays.asList(
+                new Student("S0",Arrays.asList(new Project("P0"),new Project("P1"),new Project("P2"))),
+                new Student("S1",Arrays.asList(new Project("P0"),new Project("P1"))),
+                new Student("S2",Arrays.asList(new Project("P0")))
+        );
+        TreeSet<Project> projectTreeSet= studentList.stream()
+                .flatMap(student -> student.getProjectList().stream())
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        LinkedList<Student>sortedStudents = new LinkedList<>(studentList);
+        sortedStudents.sort(Student::compareTo);
+        System.out.println("Students sorted by name: ");
+        sortedStudents.forEach(System.out::println);
+
+        System.out.println("Project sorted by name: ");
+        projectTreeSet.forEach(System.out::println);
+    }
+}
---
+++ L4/Homework/src/main/java/org/example/Project.java
@@ -0,0 +1,21 @@
+public class Project implements Comparable<Project>{
+    private final String name;
+    public Project(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public int compareTo(Project o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
